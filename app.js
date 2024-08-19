4f3d25e8b66e2a0b5e9467186e985732848d130b
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-08-18 23:28:00 -0300

    agregGraficos

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ function miMenu(){
 
 //*Graficos tipo pie Plotly js ***//
 
-const arrayx = ["Polos negros", "zapatos marrones","lentes rosas","sombreros azules","camisa verde"];
-const arrayy = ["35","23","15","20","37"];
+const arrayx = ["Manzanas", "Bananas","Peras","Guindas","Uvas"];
+const arrayy = ["25","25","15","20","10"];
 
 const titulo ={title: "stock de productos"};
 
 const datos = [{labels: arrayx, values:arrayy, type:"pie"}];
 Plotly.newPlot("miplot", datos, titulo);
+
+
+const arrayxx = ["Maas", "Banaas","Pera","Gundas","Uvs"];
+const arrayyy = ["25","25","15","20","10"];
+
+const titulo2={title: "stock de productos"};
+
+const datos2 = [{labels: arrayxx, values:arrayyy, type:"pie"}];
+Plotly.newPlot("miplot2", datos2, titulo2);
